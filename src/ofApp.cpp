9030f37f39ab50d021a54799236f1e3bd1b66be3
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2022-12-06 22:54:59 +0900

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,17 +2,33 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    minDistance = 10;
+    leftMouseButtonPressed = false;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    if (leftMouseButtonPressed) {
+        ofPoint mousePos(ofGetMouseX(), ofGetMouseY());
+        if (lastPoint.distance(mousePos) >= minDistance) {
+            // a.distance(b) calculates the Euclidean distance between point a and b.  It's
+            // the straight line distance between the points.
+            currentPolyline.curveTo(mousePos);
+            lastPoint = mousePos;
+        }
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofBackground(0);
+    ofSetColor(255);  // White color for saved polylines
+    for (int i=0; i<polylines.size(); i++) {
+        ofPolyline polyline = polylines[i];
+        polyline.draw();
+    }
+    ofSetColor(255,100,0);  // Orange color for active polyline
+    currentPolyline.draw();
 }
 
 //--------------------------------------------------------------
@@ -37,12 +53,22 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    if (button == OF_MOUSE_BUTTON_LEFT) {
+        leftMouseButtonPressed = true;
+        currentPolyline.curveTo(x, y);  // Remember that x and y are the location of the mouse
+        currentPolyline.curveTo(x, y);  // Necessary duplicate for first control point
+        lastPoint.set(x, y);  // Set the x and y of a ofVec2f in a single line
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    if(button == OF_MOUSE_BUTTON_LEFT){
+        leftMouseButtonPressed = false;
+        currentPolyline.curveTo(x,y);// Necessary duplicate for last control point
+        polylines.push_back(currentPolyline);
+        currentPolyline.clear();//Erase the vertices, allows us to start a new brush stroke
+    }
 }
 
 //--------------------------------------------------------------
